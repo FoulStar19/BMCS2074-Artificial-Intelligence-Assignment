578c6e5a82ef00b7bd1467d11ac2ef11e45e6d67
Star <100178695+FoulStar19@users.noreply.github.com>
2026-07-15 11:39:18 +0800

    Delete app directory

--- app/backend/detection/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Detection module for vehicle detection models
-"""
-
-from .cnn_detector import CNNDetector
-from .yolo_detector import YOLODetector
-
-__all__ = ['CNNDetector', 'YOLODetector']
